d05f00c37a7558b4b3bac7c18516f6d455b28ab2
gstarkg <45477348+gstarkg@users.noreply.github.com>
2022-09-18 18:45:16 +0800

    Add files via upload

--- hardhat-contract/constants/index.js
+++ hardhat-contract/constants/index.js
@@ -1,4 +1,4 @@
 const WHITELIST_CONTRACT_ADDRESS = "0x75dD83d8B4d9F7C0529e879c0255Ceb2475C040A";
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://dapp-nft-collection.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
